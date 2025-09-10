5243602d237df82061363e2ea34778bf7eb71fbf
ZhiAnYu <zhidkovaayu@gmail.com>
2025-09-10 22:29:06 +0400

    Add files via upload

---
+++ Future/Main.java
@@ -0,0 +1,70 @@
+import java.util.Random;
+
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        String[] texts = new String[25];
+        for (int i = 0; i < texts.length; i++) {
+            texts[i] = generateText("aab", 30_000);
+        }
+
+        final ExecutorService threadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        List<Future> futures = new ArrayList<>();
+
+        long startTs = System.currentTimeMillis(); // start time
+        for (String text : texts) {
+            Callable<Integer> logic = () -> {
+                int maxSize = 0;
+                for (int i = 0; i < text.length(); i++) {
+                    for (int j = 0; j < text.length(); j++) {
+                        if (i >= j) {
+                            continue;
+                        }
+                        boolean bFound = false;
+                        for (int k = i; k < j; k++) {
+                            if (text.charAt(k) == 'b') {
+                                bFound = true;
+                                break;
+                            }
+                        }
+                        if (!bFound && maxSize < j - i) {
+                            maxSize = j - i;
+                        }
+                    }
+                }
+                System.out.println(text.substring(0, 100) + " -> " + maxSize);
+                return maxSize;
+            };
+            Future<Integer> task = threadPool.submit(logic);
+            futures.add(task);
+
+        }
+
+        int maxValue = 0;
+        for (Future future : futures) {
+            int max = (int) future.get();
+            if (max > maxValue) {
+                maxValue = max;
+            }
+        }
+
+        threadPool.shutdown();
+
+        System.out.println("MaxSize-> " + maxValue);
+        long endTs = System.currentTimeMillis(); // end time
+
+        System.out.println("Time: " + (endTs - startTs) + "ms");
+    }
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+}
